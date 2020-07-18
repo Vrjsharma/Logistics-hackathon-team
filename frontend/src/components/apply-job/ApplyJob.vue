b98bece28a4fccac4abe1f7c88d47a5c97a50d40
Aravind Venkateswaran <z5208102@ad.unsw.edu.au>
2020-07-18 21:41:19 +1000

    pu it to right

--- frontend/src/components/apply-job/ApplyJob.vue
+++ frontend/src/components/apply-job/ApplyJob.vue
@@ -1,112 +1,113 @@
 <template>
   <div class="form-elements">
     <div class="row">
-      <div class="flex xs12">
-        <va-card :title="$t('Do you live in sydney?')">
-          <form>
-            <div class="row">
-              <div class="flex md6 xs12">
-                <va-select
-                  v-model="simpleSelectModel"
-                  textBy="description"
-                  :options="simpleOptions"
-                />
+      <div class="offset--md6 flex xs12">
+          <va-card :title="$t('Do you live in sydney?')">
+            <form>
+              <div class="row">
+                <div class="flex md6 xs12">
+                  <va-select
+                    v-model="simpleSelectModel"
+                    textBy="description"
+                    :options="simpleOptions"
+                  />
+                </div>
               </div>
-            </div>
-          </form>
-        </va-card>
-    </div>
-    <div class="flex xs12">
-      <va-card :title="'Let us know if you have any experience in these areas'">
-        <div class="flex xs12">
-          <va-card :title="$t('Creating Marketing Materials')">
-            <va-rating
-              v-model="value"
-              size="large"
-              color="info"
-            />
+            </form>
           </va-card>
-        </div>
-        <div class="flex xs12">
-          <va-card :title="$t('Social Media Content Creation')">
-            <va-rating
-              v-model="value1"
-              size="large"
-              color="info"
-            />
-          </va-card>
-        </div>
-        <div class="flex xs12">
-          <va-card :title="$t('Blogging/Writing')">
-            <va-rating
-              v-model="value2"
-              size="large"
-              color="info"
-            />
-          </va-card>
-        </div>
-        <div class="flex xs12">
-          <va-card :title="$t('Real Estate')">
-            <va-rating
-              v-model="value3"
-              size="large"
-              color="info"
-            />
-          </va-card>
-        </div>
-      </va-card>    
-    </div>
-    <div class="medium-editor flex xs12">
-          <va-card :title="$t('pitch')">
+      </div>
+      <div class="offset--md6 flex xs12">
+        <va-card :title="'Let us know if you have any experience in these areas'">
+          <div class="flex xs12">
+            <va-card :title="$t('Creating Marketing Materials')">
+              <va-rating
+                v-model="value"
+                size="large"
+                color="info"
+              />
+            </va-card>
+          </div>
+          <div class="flex xs12">
+            <va-card :title="$t('Social Media Content Creation')">
+              <va-rating
+                v-model="value1"
+                size="large"
+                color="info"
+              />
+            </va-card>
+          </div>
+          <div class="flex xs12">
+            <va-card :title="$t('Blogging/Writing')">
+              <va-rating
+                v-model="value2"
+                size="large"
+                color="info"
+              />
+            </va-card>
+          </div>
+          <div class="flex xs12">
+            <va-card :title="$t('Real Estate')">
+              <va-rating
+                v-model="value3"
+                size="large"
+                color="info"
+              />
+            </va-card>
+          </div>
+        </va-card>    
+      </div>
+      <div class="medium-editor offset--md6 flex xs12">
+            <va-card :title="$t('pitch')">
+              <div class="d-flex">
+                <va-medium-editor
+                  @initialized="handleEditorInitialization"
+                >
+                  <h1>Pitch Yourself!</h1>
+
+                  <p>
+                    Why should we hire you?
+                  </p>
+                </va-medium-editor>
+              </div>
+            </va-card>
+      </div>
+      <div class="medium-editor offset--md6 flex xs12">
+          <va-card :title="$t('About you')">
             <div class="d-flex">
               <va-medium-editor
                 @initialized="handleEditorInitialization"
               >
-                <h1>Pitch Yourself!</h1>
+                <h1>Tell us about you - keep it interesting!!</h1>
 
                 <p>
-                  Why should we hire you?
+                  Your experience, hobbies, interests, even a fun fact!
                 </p>
               </va-medium-editor>
             </div>
           </va-card>
-    </div>
-    <div class="medium-editor flex xs12">
-        <va-card :title="$t('About you')">
-          <div class="d-flex">
-            <va-medium-editor
-              @initialized="handleEditorInitialization"
-            >
-              <h1>Tell us about you - keep it interesting!!</h1>
-
-              <p>
-                Your experience, hobbies, interests, even a fun fact!
-              </p>
-            </va-medium-editor>
-          </div>
-        </va-card>
-    </div>
-    <div class="flex xs12">
-        <va-card :title="$t('Resume')">
-          <va-file-upload
-            dropzone
-            v-model="advancedList"
-          />
-        </va-card>
       </div>
-        <div class="flex xs12">
-        <va-card
-          class="larger-padding"
-        >
-          <div class="row">
-            <div class="flex">
-              <va-button color="danger"> {{ 'Submit' }}</va-button>
+      <div class="offset--md6 flex xs12">
+          <va-card :title="$t('Resume')">
+            <va-file-upload
+              dropzone
+              v-model="advancedList"
+            />
+          </va-card>
+        </div>
+          <div class="offset--md6 flex xs12">
+          <va-card
+            class="larger-padding"
+          >
+            <div class="row">
+              <div class="flex">
+                <va-button color="danger"> {{ 'Submit' }}</va-button>
+              </div>
             </div>
-          </div>
-        </va-card>
-      </div>
+          </va-card>
+        </div>
     </div>
   </div>
+
 </template>
 
 <script>
