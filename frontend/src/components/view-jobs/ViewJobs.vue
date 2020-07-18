9a5d20de3c7a05f3ef11a4758b0452e8c6503a92
Veeraj Sharma <v.sharma@student.unsw.edu.au>
2020-07-18 18:56:31 +1000

    view jobs

--- frontend/src/components/view-jobs/ViewJobs.vue
+++ frontend/src/components/view-jobs/ViewJobs.vue
@@ -1,8 +1,66 @@
 <template>
-  <div>
-  </div>
+  <div class="lists">
+    <div class="row">
+      <div class="flex xs12 lg6">
+        <va-list fit class="mb-2">
+
+          <va-list-label>
+            {{ $t('lists.companys') }}
+          </va-list-label>
+
+          <template v-for="(company, i) in companys">
+            <va-item :key="'item' + company.id" clickable @click="notify(company.name)">
+
+              <va-item-section avatar>
+                <va-avatar>
+                  <img :src="company.picture" :alt="company.name">
+                </va-avatar>
+              </va-item-section>
+
+              <va-item-section>
+                <va-item-label>
+                  {{ company.name }}
+                </va-item-label>
+
+                <va-item-label caption>
+                  {{ company.summary }}
+                </va-item-label>
+              </va-item-section>
+
+              <va-item-section side>
+                <va-button type="submit" clickable @click="onclick()" class="my-0">{{ $t('See job')}}</va-button>
+              </va-item-section>
+            </va-item>
+          </template>
+        </va-list>
+      </div>
+
+
+      </div>
+    </div>
 </template>
 
 <script>
+import data from './data.json'
 
+export default {
+  data () {
+    return {
+      companys: data.slice(0,4),
+      appBanners: false,
+      banners: false,
+      notifications: true,
+    }
+  },
+  methods: {
+    notify (name) {
+      this.showToast(`Clicked ${name}`, {
+        position: 'bottom-right',
+      })
+    },
+    onclick () {
+      this.$router.push({ name: '/seeker/apply-job' })
+    }
+  },
+}
 </script>
